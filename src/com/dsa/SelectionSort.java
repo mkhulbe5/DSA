c168379e5d7adfc41b56e07ef3fc14792b04ef86
mkhulbe5 <chandmohit163@gmail.com>
2026-01-26 16:04:50 +0530

    selectionsort adde

--- src/com/dsa/SelectionSort.java
+++ src/com/dsa/SelectionSort.java
@@ -1,5 +1,23 @@
 package com.dsa;
 
 public class SelectionSort {
+	public int[] selectionSort(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minIndex = i;
+
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+
+			int temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}
+
+		return arr;
+	}
 
 }
